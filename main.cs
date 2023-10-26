38782a46d592062261cee6bfd2dd830e1507de73
zhshimula <149047307+zhshimula@users.noreply.github.com>
2023-10-26 13:22:11 +0600

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,35 @@
+/* --- h2 heading --- */
+ .totoh2{font-weight: 600;
+           font-size: 18px;
+           background: #0194ff;
+           color: #fff;
+           padding: 10px;
+           line-height: 1.7em;
+           box-shadow: 0 2px 10px 0 #cac9c9;
+           text-align: center;
+           user-select: none;
+           cursor: pointer;}
+/* Human Verification */
+.hmv{position:relative;font-family:var(--fontBa);border-radius:10px;padding:30px 20px;margin-bottom:40px;text-align:center;overflow:hidden} .hmv::before{content:'';position:absolute;z-index:0;top:0;right:0;bottom:0;left:0;background:var(--linkB);opacity:.06} .hmv::after{content:'';width:60px;height:60px;background:rgba(0,0,0,.15);display:block;border-radius:50%;position:absolute;top:-12px;left:-12px;opacity:.1} .hmv >*{position:relative;z-index:1} .hmv .hmvH{font-size:1.2rem;font-weight:700;margin-bottom:15px} .hmv .hmvD{font-family:var(--fontB);font-size:13px;opacity:.8;display:inline-flex;align-items:center} .hmv .hmvD svg{width:13px;height:13px;margin-right:5px} .hmv:not(.alt) .button, .hmv.alt .hmvH.bef, .hmv:not(.alt) .hmvH.aft, .hmv.alt .hmvD{display:none} .drK .hmv::before{background:var(--darkBs);opacity:1}
+/* Scroll to Continue */
+.aSlT, .aSlB{display:none;align-items:center;justify-content:center;text-align:center;padding:30px 0} .aSlP{display:block;position:relative;height:40px;width:100%;display:none;align-items:center;justify-content:center;z-index:0;border-radius:var(--buttonR);overflow:hidden} .aSlP::before{content:''; position: absolute;top:0;right:0;bottom:0;left:0; background:var(--linkC);opacity:.5} .aSlW{position: absolute;top:0;bottom:0;left:0;width:0;background:var(--linkC);opacity:.6;transition:width 1s ease} .aSlP > span{position:absolute;color:#fffdfc;font-size:15px;overflow:hidden;white-space:nowrap;text-overflow:ellipsis} .aScr{display:none;position:relative;width:100%;font-family:var(--fontBa);border-radius:10px;padding:30px 20px;text-align:center;overflow:hidden} .aScr::after{content:'';width:60px;height:60px;background:rgba(0,0,0,.15);display:block;border-radius:50%;position:absolute;top:-12px;left:-12px;opacity:.1} .aScr::before{content:'';position:absolute;z-index:0;top:0;right:0;bottom:0;left:0;background:var(--linkB);opacity:.06} .aScr .hglt{color:var(--linkC)} .aScr .aScrH{position:relative;z-index:1;font-size:1.2rem;font-weight:700;margin-bottom:15px} .aScr .aScrD{position:relative;z-index:1;font-family:var(--fontB);font-size:13px;opacity:.8;display:inline-flex;align-items:center} .aScr .aScrD svg{width:13px;height:13px;margin-right:5px;stroke-width:1.5} .aSlT.vsbl, .aSlB.vsbl, .aSlT:not(.alt) .aSlP{display:flex} .aSlT.alt .aScr{display:block} .aSlT.nInt .aSlP > span{font-size:13px} .Rtl .aSlW{left:unset;right:0} .Rtl .aScr .aScrD svg{margin-right:0;margin-left:5px} .drK .aSlP::before{background:var(--darkBs);opacity:1} .drK .aSlW{background:var(--darkBa);opacity:1} .drK .aScr{background:var(--darkBs)} .drK .aScr::before{background:var(--darkBa);opacity:1} .drK .aScr .hglt{color:var(--darkU)}
+	
+/* AP Widget Code By Muhammad SA. */
+.app-icon,.app-name,.app.no-padding{position:relative}.app-box-name-heading,.app-icon,.app-name,.app.no-padding,.entry-block.entry-content.main-entry-content,h1.has-medium-font-size.no-margin,span,span.has-cyan-bluish-gray-color{-webkit-tap-highlight-color:transparent;box-sizing:inherit}.entry-block.entry-content.main-entry-content{-webkit-text-size-adjust:auto;margin-bottom:20px!important;margin-top:20px!important;margin:20px auto;max-width:750px}.app.no-padding{column-gap:15px;display:grid;grid-template-columns:90px calc(100% - 105px);grid-template-rows:90px;margin-bottom:15px;margin-top:0;padding:0;row-gap:15px;width:380.90625px}.app-icon{aspect-ratio:1/1;border-radius:15px;height:90px;width:90px}.app-name{align-self:center;width:275.90625px}h1.has-medium-font-size.no-margin{font-size:var(--wp--preset--font-size--medium)!important;line-height:25px;margin:0}span{font-weight:500}span.has-cyan-bluish-gray-color{color:var(--wp--preset--color--cyan-bluish-gray)!important;font-weight:400}span.has-small-font-size.has-cyan-bluish-gray-color.truncate{color:var(--wp--preset--color--cyan-bluish-gray)!important;-webkit-tap-highlight-color:transparent;box-sizing:inherit;display:block;font-size:var(--wp--preset--font-size--small)!important;margin-top:3px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}.ApseI{margin:25px 0;overflow-y:hidden;overflow-x:scroll;position:relative;white-space:nowrap;width:100%}.ApseI>*{display:inline-block;margin-right:5px}.ApseI>*:last-child{margin-right:0}.ApseI img{max-height:310px;max-width:unset;width:auto}.ApseI.h250 img{max-height:250px}.ApseI.h200 img{max-height:200px}.ApseI.h150 img{max-height:150px}@media screen and (max-width:500px){.ApseI img{max-height:250px}}.ApdbSD{position:relative;display:flex;justify-content:center;align-items:center;text-align:center;margin:30px auto;padding:4px 25px;border-radius:30px;line-height:0px;color:#fefefe;background-color:#3940F5;font-size:14px;font-weight:bold;width:calc(100% - 40px)}.wc-apk-btn:hover{opacity:.8}
+/* Unordered list style*/ 
+ul.totoli{list-style:none!important;list-style-type: none!important; padding: 0;} 
+ul.totoli > li{margin-bottom: 3px; position: relative; margin-left:35px}
+ul.totoli > li::before { content:'';background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' fill='none' stroke='%23DE3535' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check-circle'%3E%3Cpath d='M22 11.08V12a10 10 0 1 1-5.93-9.14'%3E%3C/path%3E%3Cpolyline points='22 4 12 14.01 9 11.01'%3E%3C/polyline%3E%3C/svg%3E");background-repeat: no-repeat;height:18px;width:18px; left:-30px;text-align:center;position:absolute; top: 3px}
+.drK ul.totoli > li::before{content:''; background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' fill='none' stroke='%23DE3535' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-check-circle'%3E%3Cpath d='M22 11.08V12a10 10 0 1 1-5.93-9.14'%3E%3C/path%3E%3Cpolyline points='22 4 12 14.01 9 11.01'%3E%3C/polyline%3E%3C/svg%3E");background-repeat:no-repeat;height:18px;width:18px;left:-30px;text-align:center;position:absolute;top:3px}
+body {
+-webkit-user-select: none !important; /* Safari */
+-moz-user-select: -moz-none !important; /* Firefox */
+-ms-user-select: none !important; /* Internet Explorer/Edge */
+user-select: none !important;
+}
+pre{
+-webkit-user-select: text !important;
+-moz-user-select: text !important;
+-ms-user-select: text !important;
+user-select: text !important;
+}
